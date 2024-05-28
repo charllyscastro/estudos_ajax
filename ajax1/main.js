02bafa870bb61a09b0461d0c0badda6f690cb438
charllys castro <charllyscastro@gmail.com>
2024-05-27 22:46:33 -0300

    001-ajax controle

--- ajax1/main.js
+++ ajax1/main.js
@@ -11,9 +11,16 @@ function btn_click() {
 
   //definir a funcao que vai atuar mediante a resposta
   pedido.onreadystatechange = function () {
-    document.getElementById("caixa").innerHTML = this.responseText;
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("caixa").innerHTML = this.responseText;
+    } else {
+      document.getElementById("caixa").innerHTML = "Tente mais tarde";
+    }
   };
 
+  //preparar o pedido
   pedido.open("GET", "dados.txt", true);
+
+  //enviar o pedido
   pedido.send();
 }
