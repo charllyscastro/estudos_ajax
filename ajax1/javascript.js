68c539604104c5be38c7d715fb5d43411b05077c
charllys castro <charllyscastro@gmail.com>
2024-05-28 22:47:10 -0300

    004-ajax mxl

--- ajax1/javascript.js
+++ ajax1/javascript.js
@@ -1,22 +1,33 @@
-function fnajax(url, funcao) {
+function executar() {
+  //ajax
   let pedido = new XMLHttpRequest();
 
   //resposta
   pedido.onreadystatechange = function () {
+    //verificar o estado da resposta
     if (this.readyState == 4 && this.status == 200) {
-      funcao(pedido);
+      //tratamento dos dados
+      let dados = this.responseXML;
+      let clientes = dados.getElementsByTagName("cliente");
+      let conteudo = "<h2>CLIENTES</h2>";
+
+      for (let i = 0; i < clientes.length; i++) {
+        conteudo +=
+          "<p>Nome: " +
+          clientes[i].getElementsByTagName("nome")[0].childNodes[0].nodeValue +
+          "</p>";
+        conteudo +=
+          "<p>Contacto: " +
+          clientes[i].getElementsByTagName("contacto")[0].childNodes[0]
+            .nodeValue +
+          "</p>";
+        conteudo += "<hr>";
+      }
+
+      document.getElementById("caixa1").innerHTML = conteudo;
     }
   };
-
-  //enviar o pedido
-  pedido.open("GET", url, true);
+  //pedido
+  pedido.open("GET", "dados.xml", true);
   pedido.send();
-}
-
-function cmd1(p) {
-  document.getElementById("caixa1").innerHTML = p.responseText;
-}
-
-function cmd2(p) {
-  document.getElementById("caixa2").innerHTML = p.responseText;
 }
