45e33ed710e711e877555d7ae63859e21b45e403
charllys castro <charllyscastro@gmail.com>
2024-05-28 23:11:37 -0300

    005-ajax via jquery

--- ajax1/javascript.js
+++ ajax1/javascript.js
@@ -1,33 +1,9 @@
-function executar() {
-  //ajax
-  let pedido = new XMLHttpRequest();
+// function executar() {
+//   $("#caixa").load("dados.txt");
+// }
 
-  //resposta
-  pedido.onreadystatechange = function () {
-    //verificar o estado da resposta
-    if (this.readyState == 4 && this.status == 200) {
-      //tratamento dos dados
-      let dados = this.responseXML;
-      let clientes = dados.getElementsByTagName("cliente");
-      let conteudo = "<h2>CLIENTES</h2>";
-
-      for (let i = 0; i < clientes.length; i++) {
-        conteudo +=
-          "<p>Nome: " +
-          clientes[i].getElementsByTagName("nome")[0].childNodes[0].nodeValue +
-          "</p>";
-        conteudo +=
-          "<p>Contacto: " +
-          clientes[i].getElementsByTagName("contacto")[0].childNodes[0]
-            .nodeValue +
-          "</p>";
-        conteudo += "<hr>";
-      }
-
-      document.getElementById("caixa1").innerHTML = conteudo;
-    }
-  };
-  //pedido
-  pedido.open("GET", "dados.xml", true);
-  pedido.send();
-}
+$(document).ready(function () {
+  $("#cmd").click(function () {
+    $("#caixa").load("dados.txt");
+  });
+});
