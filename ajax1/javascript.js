d0962e747fb9ababe1c68251433c0a79484f5472
charllys castro <charllyscastro@gmail.com>
2024-05-28 23:27:51 -0300

    006-ajax controlar resposta

--- ajax1/javascript.js
+++ ajax1/javascript.js
@@ -1,9 +1,14 @@
-// function executar() {
-//   $("#caixa").load("dados.txt");
-// }
-
 $(document).ready(function () {
   $("#cmd").click(function () {
-    $("#caixa").load("dados.txt");
+    $("#caixa").load("dadosx.txt", function (responseTxt, statusTxt, xhr) {
+      if (statusTxt == "success") {
+        alert("Aconteceu uma comunicação com sucesso.");
+      }
+
+      if (statusTxt == "error") {
+        // alert("Aconteceu uma error na comunicação.");
+        alert("Erro: " + xhr.status + ": " + xhr.statusText);
+      }
+    });
   });
 });
